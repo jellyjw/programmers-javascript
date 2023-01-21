9ef083786625897997d50518cf5108a011a4f9ef
jellyjw <wldnzzz@naver.com>
2023-01-22 00:10:47 +0900

    TIL: 알고리즘

---
+++ 문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    // 인덱스 n번째 글자를 기준으로 오름차순 정렬
+    if (a[n] > b[n]) return 1;
+    else if (a[n] < b[n]) return -1;
+    // 인덱스 n번째 글자가 같으면 사전순 정렬
+    else return a > b ? 1 : -1;
+  });
+}
+
+console.log(solution(["sun", "bed", "car"], 1)); // car, bed, sun
+console.log(solution(["abce", "abcd", "cdx"], 2)); // abcd, abce, cdx
