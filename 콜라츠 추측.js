5620f585c1541b0c5c3899ee355ff8c979ac429f
jellyjw <wldnzzz@naver.com>
2023-01-29 10:26:20 +0900

    TIL: 알고리즘

---
+++ 콜라츠 추측.js
@@ -0,0 +1,10 @@
+const solution = (num) => {
+  let answer = 0;
+  while (num !== 1 && answer !== 500) {
+    num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+    answer++;
+  }
+  return num == 1 ? answer : -1;
+};
+
+console.log(solution(6));
